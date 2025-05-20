8d43822ab90b9bf0568fc7a4561719cbcf969d2b
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-05-20 14:17:53 +0100

    updated app input

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 
 import { StyleSheet, Text, View } from 'react-native';
 import { Estate } from './Framework/Screens/Estate.js';
+import GroupList from './Framework/Screens/GroupList.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-    <Estate/>
+    {/* <Estate/> */}
+    <GroupList/>
     </View>
   );
 }
